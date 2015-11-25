46e8ad83aec45b5ce51dcb728977b8e30152e577
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-11-24 23:12:55 -0300

    Solution creation.

---
+++ Comments.Metadata2Xml/Comments.Metadata2Xml/Metadata2Xml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comments.Metadata2Xml
+{
+    public partial class Metadata2Xml : Form
+    {
+        public Metadata2Xml()
+        {
+            InitializeComponent();
+        }
+    }
+}
