6ed25ddb92c1f64c98817db5a7fbd847439ba71b
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-11-30 14:48:56 -0300

    Changed version to 0.1-alpha

--- Comments.Metadata2Xml/Properties/AssemblyInfo.cs
+++ Comments.Metadata2Xml/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("407e986c-3dae-4c5f-a235-c550169e3bf5")]
 // Version of the Assembly
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1-alpha")]
